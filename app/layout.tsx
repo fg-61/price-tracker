840b8c5ebd22f6b1d31f1e3a99925fe40edce0fc
@fatihgoren <fatihgoren61@gmail.com>
2023-12-12 17:05:50 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Price Tracker',
+  description: 'Track product prices.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
